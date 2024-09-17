cbbfbd6e87cc816b6b972eb0e064aeb81a299697
Ivan Lemeshev <ivanlemm@mail.ru>
2024-09-17 19:31:03 +0400

    animation first page and made second header

---
+++ js/desktop.js
@@ -0,0 +1,135 @@
+const backgroundGradient = document.getElementById("background-gradient");
+const ikigaiLogoText = document.getElementById("ikigai-logo-text");
+const creativeLogoText = document.getElementById("creative-logo-text");
+const ideasText = document.getElementById("animation-text");
+const centralImg = document.getElementById("central-img-first-section");
+const centralText = document.getElementById("central-text-first-section");
+
+const headerFirstSection = document.getElementById("header-first-section");
+const headerLogoFirstSection = document.getElementById("header-logo-first-section");
+const headerNavInfo = document.getElementById("nav-info-first-section");
+const headerNavServices = document.getElementById("nav-services-first-section");
+const headerNavContacts = document.getElementById("nav-contacts-first-section");
+const headerNavReviews = document.getElementById("nav-reviews-first-section");
+const headerDividingLine = document.getElementById("header-dividing-line-first-section");
+const headerLanguageButton = document.getElementById("language-button-first-section");
+
+const secondHeader = document.getElementById("second-header");
+const secondHeaderLogoHover = document.getElementById("second-header-logo-hover");
+const secondHeaderLogoActiv = document.getElementById("second-header-logo-activ");
+
+const secondHeaderNavMain = document.getElementById("second-nav-main");
+const secondHeaderNavInfo = document.getElementById("second-nav-info");
+const secondHeaderNavServices = document.getElementById("second-nav-services");
+const secondHeaderNavContacts = document.getElementById("second-nav-contacts");
+const secondHeaderNavReviews = document.getElementById("second-nav-reviews");
+const secondHeaderLanguageButton = document.getElementById("second-language-button");
+const secondHeaderDividingLine = document.getElementById("second-header-dividing-line");
+
+let animationisActiv = false;
+
+const startAnimation = () => {
+    setTimeout(() => {
+        backgroundGradient.style.opacity = "1";
+        setTimeout(() => {
+            ikigaiLogoText.style.opacity = "1";
+            creativeLogoText.style.opacity = "1";
+            setTimeout(() => {
+                ikigaiLogoText.style.left = "7.44792vw";
+                creativeLogoText.style.left = "73.48958vw";
+                setTimeout(() => {
+                    ideasText.style.opacity = "1";
+                    setTimeout(() => {
+                        centralImg.style.opacity = "1";
+                        centralImg.style.rotate = "360deg";
+                        ikigaiLogoText.style.opacity = "0.5";
+                        creativeLogoText.style.opacity = "0.5";
+                        setTimeout(() => {
+                            ideasText.style.opacity = "0";
+                            centralText.style.opacity = "1";
+                            setTimeout(() => {
+                                headerFirstSection.style.opacity = "1";
+                                setTimeout(() => {
+                                    headerLogoFirstSection.style.opacity = "1";
+                                    setTimeout(() => {
+                                        headerNavInfo.style.opacity = "1";
+                                        setTimeout(() => {
+                                            headerNavServices.style.opacity = "1";
+                                            setTimeout(() => {
+                                                headerNavContacts.style.opacity = "1";
+                                                setTimeout(() => {
+                                                    headerNavReviews.style.opacity = "1";
+                                                    setTimeout(() => {
+                                                        headerDividingLine.style.opacity = "1";
+                                                        headerLanguageButton.style.opacity = "1";
+                                                    }, 300);
+                                                }, 300);
+                                            }, 300);
+                                        }, 300);
+                                    }, 300);
+                                }, 300);
+                            }, 1200);
+                        }, 1100);
+                    }, 900);
+                }, 700);
+            }, 600);
+        }, 400);
+    }, 200);
+};
+
+window.onload = startAnimation;
+
+window.addEventListener('scroll', () => {
+    if (!animationisActiv) {
+        if (window.scrollY > window.innerHeight) {
+            animationisActiv = true;
+            secondHeader.style.display = "flex";
+            setTimeout(() => {
+                secondHeader.style.opacity = "1";
+                animationisActiv = false;
+            }, 1);
+        } else {
+            animationisActiv = true;
+            secondHeader.style.opacity = "0";
+            setTimeout(() => {
+                secondHeader.style.display = "none";
+                animationisActiv = false;
+            }, 300);
+        }
+    }
+});
+
+secondHeaderLogoHover.addEventListener('click', () => {
+    secondHeaderLogoHover.style.opacity = "0";
+    secondHeaderLogoActiv.style.opacity = "1";
+    secondHeaderNavMain.style.opacity = "1";
+    secondHeaderNavInfo.style.opacity = "1";
+    secondHeaderNavServices.style.opacity = "1";
+    secondHeaderNavContacts.style.opacity = "1";
+    secondHeaderNavReviews.style.opacity = "1";
+    secondHeaderLanguageButton.style.opacity = "1";
+    secondHeaderDividingLine.style.opacity = "1";
+});
+
+secondHeader.addEventListener('mouseleave', () => {
+    secondHeaderLogoHover.style.opacity = "0.5";
+    secondHeaderLogoActiv.style.opacity = "0";
+    secondHeaderNavMain.style.opacity = "0";
+    secondHeaderNavInfo.style.opacity = "0";
+    secondHeaderNavServices.style.opacity = "0";
+    secondHeaderNavContacts.style.opacity = "0";
+    secondHeaderNavReviews.style.opacity = "0";
+    secondHeaderLanguageButton.style.opacity = "0";
+    secondHeaderDividingLine.style.opacity = "0";
+});
+
+secondHeaderLogoHover.addEventListener('mouseenter', () => {
+    secondHeaderLogoHover.style.opacity = "1";
+});
+
+window.addEventListener('scroll', () => {
+    if( window.scrollY > window.innerHeight) {
+        secondHeaderNavInfo.style.opacity = "0.5"
+    }
+});
+        
