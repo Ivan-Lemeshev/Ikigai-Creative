d0dc9561142dda0e5ec31c77881e6b2328af9def
Ivan Lemeshev <ivanlemm@mail.ru>
2024-12-06 21:42:02 +0400

    localization

--- js/desktop.js
+++ js/desktop.js
@@ -1246,29 +1246,52 @@ fourthSectionChoiceOfService.addEventListener('click', (event) => {
     const newDropListWrapper = document.createElement('div');
     newDropListWrapper.id = "drop-list";
     dropListWrapper.style.height = "17.5vw"
-    const actualityCategory = arrayDataOfService.map((service) => service.title).filter((textCategory) => {
-        return textCategory !== selectCategory;
-    });
 
-    actualityCategory.forEach(textSelectCategoty => {
-        const newDropListPoint = document.createElement('div');
-        newDropListPoint.textContent = textSelectCategoty;
-        newDropListPoint.classList = "drop-list-item";
-        newDropListPoint.id = textSelectCategoty;
-
-        newDropListPoint.addEventListener('click', (event) => {
-            event.stopPropagation();
-            selectCategory = textSelectCategoty;
-            choiceOfServicePlaceholder.textContent = textSelectCategoty;
-            newDropListWrapper.style.opacity = "0";
-            fourthSectionChoiceOfServiceArrow.style.rotate = "0deg";
-            fourthSectionChoiceOfService.style.borderBottomColor = "rgba(2, 133, 204, 1)";
-            newDropListWrapper.style.pointerEvents = "none";
-            allInputsHave();
+    if (selectLang === "eng") {
+        const actualityCategory = arrayDataOfService.map((service) => service.title.eng).filter((textCategory) => {
+            return textCategory !== selectCategory;
         });
-        newDropListWrapper.appendChild(newDropListPoint);
+        actualityCategory.forEach(textSelectCategoty => {
+            const newDropListPoint = document.createElement('div');
+            newDropListPoint.textContent = textSelectCategoty;
+            newDropListPoint.classList = "drop-list-item";
+            newDropListPoint.id = textSelectCategoty;
 
-    });
+            newDropListPoint.addEventListener('click', (event) => {
+                event.stopPropagation();
+                selectCategory = textSelectCategoty;
+                choiceOfServicePlaceholder.textContent = textSelectCategoty;
+                newDropListWrapper.style.opacity = "0";
+                fourthSectionChoiceOfServiceArrow.style.rotate = "0deg";
+                fourthSectionChoiceOfService.style.borderBottomColor = "rgba(2, 133, 204, 1)";
+                newDropListWrapper.style.pointerEvents = "none";
+                allInputsHave();
+            });
+            newDropListWrapper.appendChild(newDropListPoint);
+        });
+    } else {
+        const actualityCategory = arrayDataOfService.map((service) => service.title.rus).filter((textCategory) => {
+            return textCategory !== selectCategory;
+        });
+        actualityCategory.forEach(textSelectCategoty => {
+            const newDropListPoint = document.createElement('div');
+            newDropListPoint.textContent = textSelectCategoty;
+            newDropListPoint.classList = "drop-list-item";
+            newDropListPoint.id = textSelectCategoty;
+
+            newDropListPoint.addEventListener('click', (event) => {
+                event.stopPropagation();
+                selectCategory = textSelectCategoty;
+                choiceOfServicePlaceholder.textContent = textSelectCategoty;
+                newDropListWrapper.style.opacity = "0";
+                fourthSectionChoiceOfServiceArrow.style.rotate = "0deg";
+                fourthSectionChoiceOfService.style.borderBottomColor = "rgba(2, 133, 204, 1)";
+                newDropListWrapper.style.pointerEvents = "none";
+                allInputsHave();
+            });
+            newDropListWrapper.appendChild(newDropListPoint);
+        });
+    }
 
     newDropListWrapper.style.opacity = "1";
     fourthSectionChoiceOfServiceArrow.style.rotate = "-90deg";
@@ -1296,12 +1319,21 @@ fourthSectionChoiceOfService.addEventListener('click', (event) => {
 let numberOfService = 0;
 const textarea = document.getElementById("textarea");
 
-const textForApplication = [
-    "",
-    "Hello, I want to get advice on the chosen service",
-    "Hello, I want to get an answer to the question:",
-    "Hello, I have used this service and I want to leave a review:"
-]
+const textForApplication = {
+    eng: [
+        "",
+        "Hello, I want to get advice on the chosen service",
+        "Hello, I want to get an answer to the question:",
+        "Hello, I have used this service and I want to leave a review:"
+    ],
+
+    rus: [
+        "",
+        "Здравстуйте, я хочу воспользоваться выбранной услугой",
+        "Здравстуйте, я хочу задать вам несколько вопросов",
+        "Здравстуйте, я воспользовался вашей услугой и хочу оставить о ней отзыв"
+    ]
+}
 
 const setupAndTranslateForSelectService = (event) => {
     event.preventDefault();
@@ -1310,19 +1342,31 @@ const setupAndTranslateForSelectService = (event) => {
         behavior: 'smooth'
     });
 
-    textarea.textContent = textForApplication[numberOfService];
-    let selCat = arrayDataOfService.map((service) => service.title)[activNumber];
-
-    if (selCat !== selectCategory && arrayDataOfService.map((service) => service.title).includes(selCat)) {
-        choiceOfServicePlaceholder.textContent = selCat;
-        fourthSectionChoiceOfServiceArrow.style.rotate = "0deg";
-        fourthSectionChoiceOfService.style.borderBottomColor = "rgba(2, 133, 204, 1)";
-        choiceOfServicePlaceholder.style.color = "var(--blue)";
-        selectCategory = selCat;
-        allInputsHave();
+    if (selectLang === "eng") {
+        textarea.textContent = textForApplication.eng[numberOfService];
+        console.log(textarea.textContent)
+        let selCat = arrayDataOfService.map((service) => service.title.eng)[activNumber];
+        if (selCat !== selectCategory && arrayDataOfService.map((service) => service.title.eng).includes(selCat)) {
+            choiceOfServicePlaceholder.textContent = selCat;
+            fourthSectionChoiceOfServiceArrow.style.rotate = "0deg";
+            fourthSectionChoiceOfService.style.borderBottomColor = "rgba(2, 133, 204, 1)";
+            choiceOfServicePlaceholder.style.color = "var(--blue)";
+            selectCategory = selCat;
+            allInputsHave();
+        }
+    } else {
+        textarea.textContent = textForApplication.rus[numberOfService];
+        let selCat = arrayDataOfService.map((service) => service.title.rus)[activNumber];
+        if (selCat !== selectCategory && arrayDataOfService.map((service) => service.title.rus).includes(selCat)) {
+            choiceOfServicePlaceholder.textContent = selCat;
+            fourthSectionChoiceOfServiceArrow.style.rotate = "0deg";
+            fourthSectionChoiceOfService.style.borderBottomColor = "rgba(2, 133, 204, 1)";
+            choiceOfServicePlaceholder.style.color = "var(--blue)";
+            selectCategory = selCat;
+            allInputsHave();
+        }
     }
 }
-
 
 for (let index = 0; index < textForApplication.length; index++) {
     const button = document.getElementById(`button-more-lower-part-buttons-${index}`);
@@ -1522,43 +1566,109 @@ buttonWrapper.addEventListener('mouseleave', () => {
 const reviews = [
     {
         top: {
-            title: "Dmitry Malyukin",
-            upperText: "In 2022, our project scaled up. We turned to the team at Ikigai Creative to update our brand identity. Time, as it turned out, has proven the success of the innovations they introduced. Working with this agency has been a real pleasure. My colleagues and I quickly decided to continue a long-term collaboration, which is still ongoing to this day.",
-            lowerText: "I can recommend this organization for their high-quality and diligent work over many years."
+            title: {
+                eng: "Dmitry Malyukin",
+                rus: "Дмитрий Малюкин"
+            },
+
+            upperText: {
+                eng: "In 2022, our project scaled up. We turned to the team at Ikigai Creative to update our brand identity. Time, as it turned out, has proven the success of the innovations they introduced. Working with this agency has been a real pleasure. My colleagues and I quickly decided to continue a long-term collaboration, which is still ongoing to this day.",
+                rus: "Мы обратились к команде Ikigai Creative с просьбой обновить фирменный стиль нашего бренда. Время доказало успех внедренных ими инноваций. Мы с моими коллегами быстро решили продолжить долгосрочное сотрудничество, которое продолжается и по сей день."
+            },
+
+            lowerText: {
+                eng: "I can recommend this organization for their high-quality and diligent work over many years.",
+                rus: "Хочу порекомендовать эту организацию за ее качественную и добросовестную работу"
+            }
         },
 
         bottom: {
-            title: "Mikhail Potapenko",
-            upperText: "When my team decided to update our website for our volunteer project, I didn’t expect it to turn out this great! The team really understood our vision and added some brilliant ideas we hadn’t thought of. Everything was done quickly and with great attention to detail. Now, our website not only works but inspires more people to get involved.",
-            lowerText: "My admiration and respect."
+            title: {
+                eng: "Mikhail Potapenko",
+                rus: "Михаил Потапенко"
+            },
+
+            upperText: {
+                eng: "When my team decided to update our website for our volunteer project, I didn’t expect it to turn out this great! The team really understood our vision and added some brilliant ideas we hadn’t thought of. Everything was done quickly and with great attention to detail. Now, our website not only works but inspires more people to get involved.",
+                rus: "Наша компания энтузиастов мотоспорта хотела продвигать движение в Москве. Мы обратились  за дизайном, разработкой и продвижением веб-сайта. Компания отнеслась к проекту очень серьезно и быстро создали не только отличный макет, но и сам веб-сайт! Ikigai также помогал нам создавать контент для наших групп — писать посты, снимать видео! На нас, клиентов, было делигированно никаких задач — они обо всем позаботились."
+            },
+
+            lowerText: {
+                eng: "My admiration and respect.",
+                rus: "Мое восхищение и уважение."
+            }
         }
     },
 
     {
         top: {
-            title: "Diana Belousova",
-            upperText: "Our motorwheel enthusiasts' company wanted to promote our movement in Moscow. We approached this company for website design, development, and promotion. They took the project very seriously and quickly delivered not only an excellent mockup but also the website itself! Ikigai also helped us manage content for our groups—writing posts, shooting videos! As clients, we didn’t have to handle any unnecessary tasks—they took care of everything.",
-            lowerText: "I highly recommend them as partners!"
+            title: {
+                eng: "Diana Belousova",
+                rus: "Диана Белоусова"
+            },
+
+            upperText: {
+                eng: "Our motorwheel enthusiasts' company wanted to promote our movement in Moscow. We approached this company for website design, development, and promotion. They took the project very seriously and quickly delivered not only an excellent mockup but also the website itself! Ikigai also helped us manage content for our groups—writing posts, shooting videos! As clients, we didn’t have to handle any unnecessary tasks—they took care of everything.",
+                rus: "Когда моя команда решила обновить сайт для нашего волонтёрского проекта, я не ожидала, что всё получится так здорово! Команда отлично поняла нашу идею и предложила несколько потрясающих решений. Всё было сделано быстро и с вниманием к каждой детали. Теперь наш сайт не только работает, но и вдохновляет больше людей присоединяться."
+            },
+
+            lowerText: {
+                eng: "I highly recommend them as partners!",
+                rus: "Рекомендую их как надёжных партнёров!"
+            }
         },
 
         bottom: {
-            title: "Andrey Skugarov",
-            upperText: "As a guy who knows how to enjoy the good things in life, I never expected our project to turn out this well. I reached out for a video production project, and they didn’t just meet my expectations—they exceeded them. From the start, they understood my vision, and the results were outstanding. The video captured our brand perfectly, with every detail in place. They didn’t just shoot and edit—they brought our vision to life in ways I hadn’t imagined.",
-            lowerText: "Respect to this team for their amazing work!"
+            title: {
+                eng: "Andrey Skugarov",
+                rus: "Андрей Скугаров"
+            },
+
+            upperText: {
+                eng: "As a guy who knows how to enjoy the good things in life, I never expected our project to turn out this well. I reached out for a video production project, and they didn’t just meet my expectations—they exceeded them. From the start, they understood my vision, and the results were outstanding. The video captured our brand perfectly, with every detail in place. They didn’t just shoot and edit—they brought our vision to life in ways I hadn’t imagined.",
+                rus: "Как человек, который умеет ценить хорошие вещи в жизни, я не ожидал, что наш проект получится настолько успешным. Я обратился к ним для создания видео, и они не просто оправдали мои ожидания — они их превзошли. Видео идеально отразило суть нашего бренда, каждая деталь была на своём месте. Они не просто сняли и смонтировали — они воплотили нашу идею в жизнь так, как я и не мог представить."
+            },
+
+            lowerText: {
+                eng: "Respect to this team for their amazing work!",
+                rus: "Респект этой команде за отличную работу!"
+            } 
         }
     },
 
     {
         top: {
-            title: "Sofia Pomina",
-            upperText: "When my international orders started increasing, I knew I needed a more efficient and reliable way to manage payments. I reached out to this team, and they quickly set up a secure and user-friendly payment system that fits my needs perfectly. Now, the process is seamless, and I don’t have to worry about complicated transactions anymore, giving me more time to focus on growing my business.",
-            lowerText: "Highly recommend them for reliable payment solutions!"
+            title: {
+                eng: "Sofia Pomina",
+                rus: "София Поминова"
+            },
+
+            upperText: {
+                eng: "When my international orders started increasing, I knew I needed a more efficient and reliable way to manage payments. I reached out to this team, and they quickly set up a secure and user-friendly payment system that fits my needs perfectly. Now, the process is seamless, and I don’t have to worry about complicated transactions anymore, giving me more time to focus on growing my business.",
+                rus: "Когда количество международных заказов начало расти, я поняла, что мне нужно более удобное и надежное решение для управления платежами. Обратившись к этой команде, я была приятно удивлена — они быстро настроили безопасную и простую в использовании платежную систему, которая идеально подошла для моих нужд. "
+            },
+
+            lowerText: {
+                eng: "Highly recommend them for reliable payment solutions!",
+                rus: "Они точно знают толк в этом деле"
+            } 
         },
 
         bottom: {
-            title: "Yulia Biryukova",
-            upperText: "As a dancer who’s always looking to stand out, I never expected my social media to blow up like it did. I reached out to this team for social media promotion, and they didn’t just deliver—they totally elevated my online presence. From the start, they understood my vibe and nailed it. My content has never looked better, and the engagement is off the charts. They didn’t just manage my posts—they made my brand shine in ways I never thought possible.",
-            lowerText: "Can’t thank them enough for helping me level up!"
+            title: {
+                eng: "Yulia Biryukova",
+                rus: "Юлия Бирюкова"
+            },
+
+            upperText: {
+                eng: "As a dancer who’s always looking to stand out, I never expected my social media to blow up like it did. I reached out to this team for social media promotion, and they didn’t just deliver—they totally elevated my online presence. From the start, they understood my vibe and nailed it. My content has never looked better, and the engagement is off the charts. They didn’t just manage my posts—they made my brand shine in ways I never thought possible.",
+                rus: "Как танцовщица, всегда стремящаяся быть на виду, я не ожидала, что моё продвижение в соцсетях принесёт такие результаты. Обратившись к этой команде, я сразу поняла, что сделала правильный выбор. Они не просто подошли к моему стилю с вниманием, но и предложили идеи, которые значительно улучшили моё онлайн-присутствие. Мой контент стал ярче, а вовлеченность и интерес к моей работе возросли."
+            },
+
+            lowerText: {
+                eng: "Can’t thank them enough for helping me level up!",
+                rus: "Огромное спасибо за помощь в продвижении!"
+            } 
         }
     }
 ]
@@ -1603,14 +1713,23 @@ const upperloadReviews = (numberActivReviews, direction) => {
             float = false;
         }, 500);
         setTimeout(() => {
-            reviewsUpperName.textContent = data.top.title;
-            reviewsUpperImg.src = `/img/png/reviews-img-${data.top.title}.png`;
-            reviewsUpperUpperText.textContent = data.top.upperText;
-            reviewsUpperLowerText.textContent = data.top.lowerText;
-            reviewsLowerName.textContent = data.bottom.title;
-            reviewsLowerImg.src = `/img/png/reviews-img-${data.bottom.title}.png`;
-            reviewsLowerUpperText.textContent = data.bottom.upperText;
-            reviewsLowerLowerText.textContent = data.bottom.lowerText;
+            if(selectLang === "eng") {
+                reviewsUpperName.textContent = data.top.title.eng;
+                reviewsUpperUpperText.textContent = data.top.upperText.eng;
+                reviewsUpperLowerText.textContent = data.top.lowerText.eng;
+                reviewsLowerName.textContent = data.bottom.title.eng;
+                reviewsLowerUpperText.textContent = data.bottom.upperText.eng;
+                reviewsLowerLowerText.textContent = data.bottom.lowerText.eng;
+            } else {
+                reviewsUpperName.textContent = data.top.title.rus;
+                reviewsUpperUpperText.textContent = data.top.upperText.rus;
+                reviewsUpperLowerText.textContent = data.top.lowerText.rus;
+                reviewsLowerName.textContent = data.bottom.title.rus;
+                reviewsLowerUpperText.textContent = data.bottom.upperText.rus;
+                reviewsLowerLowerText.textContent = data.bottom.lowerText.rus;
+            }
+            reviewsUpperImg.src = `/img/png/reviews-img-${data.top.title.eng}.png`;
+            reviewsLowerImg.src = `/img/png/reviews-img-${data.bottom.title.eng}.png`;
         }, 400);
     }
 }
