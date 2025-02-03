f391c6e3daf2692b2d09a6fbbb12958a56280bd9
Ivan Lemeshev <ivanlemm@mail.ru>
2025-02-03 15:32:12 +0400

    links

--- public/js/links.js
+++ public/js/links.js
@@ -2,7 +2,7 @@ const dataContats = {
     Phone: "+44 (773) 779-12-71",
     Email: "info.speakingbritain@gmail.com",
     Facebook: "https://www.facebook.com/share/18TohucDbf/",
-    Telegram: "@KramarenkoIvan",
+    Telegram: "https://t.me/KramarenkoIvan",
 
 }
 
